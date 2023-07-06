7d29e875b6ed63eb6ea1434e51971cc3b8a4fe8b
Arti-Dev <boomcreeper296@gmail.com>
2023-07-05 21:02:47 -0400

    Add new "Speedtech" feature
    It just measures the angle between your movement and your player direction.

--- src/main/java/com/articreep/pocketknife/Speedtech.java
+++ src/main/java/com/articreep/pocketknife/Speedtech.java
@@ -22,10 +22,12 @@ public class Speedtech extends PocketknifeSubcommand implements PocketknifeFeatu
     private boolean enabled = false;
     private boolean lines = false;
     private int taskID = -1;
+    private final Map<Player, Float> initialSpeeds = new HashMap<>();
+    private final Map<Player, Integer> speedBonuses = new HashMap<>();
+    private final Map<Player, Double> previousAngles = new HashMap<>();
     // PlayerMoveEvent.getFrom() is not being reliable, so...
     // Honestly this is pretty bad either way
     private final Map<Player, Location> previousLocations = new HashMap<>();
-    private final Map<Player, Location> currentLocations = new HashMap<>();
 
 
     @Override
@@ -92,15 +94,16 @@ public class Speedtech extends PocketknifeSubcommand implements PocketknifeFeatu
     @EventHandler
     public void onMove(PlayerMoveEvent event) {
         Player player = event.getPlayer();
-        if (enabled) {
-            // nothing here yet
-        }
         if (lines) {
             drawLines(player, previousLocations.get(player), player.getLocation());
         }
     }
 
     private void drawLines(Player player, Location prevLoc, Location currentLoc) {
+        // Clone before we do anything
+        prevLoc = prevLoc.clone();
+        currentLoc = currentLoc.clone();
+
         World w = player.getWorld();
         boolean ignoreMovement = false;
 
@@ -110,7 +113,10 @@ public class Speedtech extends PocketknifeSubcommand implements PocketknifeFeatu
         Vector movementDir = currentLoc.subtract(prevLoc).toVector().setY(0);
         Vector playerDir = player.getLocation().getDirection().setY(0);
 
-        if (movementDir.isZero()) ignoreMovement = true;
+        if (movementDir.isZero()) {
+            player.sendMessage("Movement dir is zero");
+            ignoreMovement = true;
+        }
 
         double inbetween = 0.2;
         double distance = 10;
@@ -141,17 +147,111 @@ public class Speedtech extends PocketknifeSubcommand implements PocketknifeFeatu
     private int trackLocations() {
         BukkitTask task = Bukkit.getScheduler().runTaskTimer(Pocketknife.getInstance(), () -> {
             for (Player player : Bukkit.getOnlinePlayers()) {
-                if (currentLocations.containsKey(player)) {
-                    Location oldLoc = currentLocations.get(player);
-                    previousLocations.put(player, oldLoc);
-                    currentLocations.put(player, player.getLocation());
-                } else {
-                    currentLocations.put(player, player.getLocation());
-                    previousLocations.put(player, player.getLocation());
+
+                Location oldLoc;
+                Location newLoc = player.getLocation();
+                if (!previousLocations.containsKey(player)) {
+                    previousLocations.put(player, newLoc);
                 }
+                oldLoc = previousLocations.get(player);
+
+//                double oldAngle;
+//                double newAngle = calculateAngle(player, oldLoc, newLoc);
+//                if (!previousAngles.containsKey(player)) {
+//                    previousAngles.put(player, newAngle);
+//                }
+//                oldAngle = previousAngles.get(player);
+
+//                /* Conditions to gain speed:
+//                - Player must be sprinting
+//                - Angle must deviate at least 10 degrees from its previous measurement (previous tick)
+//                - Angle must be between 10 and 35 degrees
+//
+//                Speed will build up linearly at a rate of 0.007.
+//                (Default speed is 0.2, max speed is 1)
+//                A successful tick adds 1 to the speed variable.
+//                Players need at least 5 on this meter before gaining any speed.
+//
+//                Speed will reset to its initial value if:
+//                - Player instantly comes to a stop (previous location == current location)
+//                This is not lag-friendly, but that's okay.
+//                */
+//
+//                double thresholdMin = 10;
+//                double thresholdMax = 35;
+//                double deviate = 10;
+//                double rate = 0.007;
+//
+//                // Conditions to gain speed
+//                if (player.isSprinting() && Math.abs(oldAngle - newAngle) >= deviate
+//                        && thresholdMin <= newAngle && newAngle <= thresholdMax) {
+//
+//                    // Add speed bonus
+//                    if (!speedBonuses.containsKey(player)) {
+//                        initialSpeeds.put(player, player.getWalkSpeed());
+//                        speedBonuses.put(player, 1);
+//                    } else {
+//                        speedBonuses.put(player, speedBonuses.get(player) + 1);
+//                    }
+//
+//                    player.sendMessage("Speeding up...");
+//
+//                    int bonus = speedBonuses.get(player);
+//                    float initialSpeed = initialSpeeds.get(player);
+//                    float speed;
+//                    // Calculate walk speed
+//                    if (bonus > 5) {
+//                        speed = (float) (initialSpeed + (rate * (bonus - 5)));
+//                    } else {
+//                        // do not apply bonuses yet
+//                        speed = initialSpeed;
+//                    }
+//
+//                    if (speed < initialSpeed) speed = initialSpeed;
+//                    if (speed > 1) speed = 1;
+//
+//                    player.setWalkSpeed(speed);
+//
+//                }
+//
+//                // Conditions to lose all speed
+//                if (speedBonuses.containsKey(player)) {
+//                    if (oldLoc.equals(newLoc)) {
+//                        speedBonuses.remove(player);
+//                        player.setWalkSpeed(initialSpeeds.get(player));
+//                        initialSpeeds.remove(player);
+//                    }
+//                }
+
+
+                // Operation's over, prepare for the next tick
+//                previousAngles.put(player, newAngle);
+                player.sendMessage(newLoc.toString());
+                player.sendMessage(oldLoc.toString());
+                previousLocations.put(player, newLoc);
             }
         }, 0, 1);
 
         return task.getTaskId();
+    }
+
+    private double calculateAngle(Player player, Location prevLoc, Location currentLoc) {
+        // Clone before we use them
+        prevLoc = prevLoc.clone();
+        currentLoc = currentLoc.clone();
+
+        World w = player.getWorld();
+
+        if (currentLoc.getWorld() != w || prevLoc.getWorld() != w) return -1;
+
+        // Ignore y coordinates here
+        Vector movementDir = currentLoc.subtract(prevLoc).toVector().setY(0);
+        Vector playerDir = player.getLocation().getDirection().setY(0);
+
+        if (movementDir.isZero()) return -1;
+
+        double angle = movementDir.angle(playerDir);
+        angle = angle * (180/Math.PI);
+        return angle;
     }
 }
