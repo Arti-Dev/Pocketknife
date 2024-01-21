796f7413f2f18fadb08c406a548df008a659a425
Arti-Dev <boomcreeper296@gmail.com>
2024-01-20 20:04:41 -0500

    Add SplashDecoratedPot feature

---
+++ src/main/java/com/articreep/pocketknife/features/SplashDecoratedPot.java
@@ -0,0 +1,58 @@
+package com.articreep.pocketknife.features;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.DecoratedPot;
+import org.bukkit.entity.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class SplashDecoratedPot implements Listener {
+    /*
+    When a decorated pot containing a splash/lingering potion is broken
+    the potion comes out as a thrown potion
+     */
+    @EventHandler
+    public void onPotBreak(ProjectileHitEvent event) {
+        Block block = event.getHitBlock();
+        if (block.getState() instanceof DecoratedPot pot) {
+            if (!canBreakDecoratedPot(event.getEntity())) return;
+            handleItemInPot(pot);
+        }
+    }
+
+    @EventHandler
+    public void onPotBreak(BlockBreakEvent event) {
+        Block block = event.getBlock();
+        if (block.getState() instanceof DecoratedPot pot) {
+            handleItemInPot(pot);
+        }
+    }
+
+    private void handleItemInPot(DecoratedPot pot) {
+        ItemStack droppedItem = pot.getInventory().getItem();
+        if (droppedItem == null) return;
+        if (droppedItem.getType() == Material.SPLASH_POTION || droppedItem.getType() == Material.LINGERING_POTION) {
+            ThrownPotion potion = (ThrownPotion) pot.getWorld().spawnEntity(pot.getLocation(), EntityType.SPLASH_POTION);
+            potion.setItem(droppedItem);
+            // empty the pot before it's broken
+            pot.getInventory().setItem(new ItemStack(Material.AIR));
+        }
+    }
+
+    /**
+     * This is obviously very limited - I haven't tested EVERY projectile.
+     * @param projectile
+     * @return true if the projectile can break decorated pots
+     */
+    public static boolean canBreakDecoratedPot(Projectile projectile) {
+        if (projectile instanceof Arrow) return true;
+        if (projectile instanceof Snowball) return true;
+        if (projectile instanceof Egg) return true;
+        if (projectile instanceof Firework) return true;
+        else return false;
+    }
+}
